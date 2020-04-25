0d8cb7438f2cfaed740ae38b205ffc332f74b0f9
Devone Austin <devone.austin@gmail.com>
2020-04-25 01:56:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -45,7 +45,7 @@ class CashRegister
   end
 
   def void_last_transaction
-    self.total = self.total - self.last_transaction_amount
+    self.total = (total * ((100.0 - discount.to_f)/100)).to_i 
   end
 
 end
